256c32063b6b8d8feb671c66c00e2fea36653f70
Monho12 <92088341+Monho12@users.noreply.github.com>
2023-02-11 12:55:16 +0800

    sidebar updated

--- bill-gates/src/Components/Sidebar.jsx
+++ bill-gates/src/Components/Sidebar.jsx
@@ -1,16 +1,9 @@
 import style from "../Designs/Sidebar.module.css";
 import logo from "../Assets/Logo.svg";
-import home from "../Assets/Home.svg";
-import not from "../Assets/Not.svg";
-import pfp from "../Assets/Profile.svg";
-import list from "../Assets/Lists.svg";
-import book from "../Assets/Book.svg";
-import msg from "../Assets/Msg.svg";
-import exp from "../Assets/Expl.svg";
-import more from "../Assets/More.svg";
 import picP from "../Assets/Profile Picture.svg";
 import dots from "../Assets/dots.svg";
 import { Link } from "react-router-dom";
+import { Sections } from "./json/sidebar";
 
 export const Sidebar = () => {
   return (
@@ -25,60 +18,20 @@ export const Sidebar = () => {
         </div>
       </Link>
       <div className={style.sections}>
-        <Link to="/" style={{ textDecoration: "none", color: "black" }}>
-          <div className={style.section}>
-            <img src={home} alt="home" />
-            <div className={style.text}>Home</div>
-          </div>
-        </Link>
-        <Link to="/explore" style={{ textDecoration: "none", color: "black" }}>
-          <div className={style.section}>
-            <img src={exp} alt="exp" />
-            <div className={style.text}>Explore</div>
-          </div>
-        </Link>
-        <Link
-          to="/notifications"
-          style={{ textDecoration: "none", color: "black" }}
-        >
-          <div className={style.section}>
-            <img src={not} alt="not" />
-            <div className={style.text}>Notifications</div>
-          </div>
-        </Link>
-        <Link to="/messages" style={{ textDecoration: "none", color: "black" }}>
-          <div className={style.section}>
-            <img src={msg} alt="msg" />
-            <div className={style.text}>Messages</div>
-          </div>
-        </Link>
-        <Link
-          to="/bookmarks"
-          style={{ textDecoration: "none", color: "black" }}
-        >
-          <div className={style.section}>
-            <img src={book} alt="book" />
-            <div className={style.text}>Bookmarks</div>
-          </div>
-        </Link>
-        <Link to="/lists" style={{ textDecoration: "none", color: "black" }}>
-          <div className={style.section}>
-            <img src={list} alt="list" />
-            <div className={style.text}>Lists</div>
-          </div>
-        </Link>
-        <Link to="/profile" style={{ textDecoration: "none", color: "black" }}>
-          <div className={style.section}>
-            <img src={pfp} alt="pfp" />
-            <div className={style.text}>Profile</div>
-          </div>
-        </Link>
-        <Link to="/more" style={{ textDecoration: "none", color: "black" }}>
-          <div className={style.section}>
-            <img src={more} alt="more" />
-            <div className={style.text}>More</div>
-          </div>
-        </Link>
+        {Sections.map((item, index) => {
+          return (
+            <Link
+              to={`/${item.path}`}
+              style={{ textDecoration: "none", color: "black" }}
+            >
+              <div className={style.section}>
+                <img src={item.icon} alt={`${item.name}`} />
+                <div className={style.text}>{item.name}</div>
+              </div>
+            </Link>
+          );
+        })}
+
         <div className={style.section}>
           <button className={style.button}>Tweet</button>
         </div>
---
+++ bill-gates/src/Components/json/sidebar.js
@@ -0,0 +1,50 @@
+import home from "../../Assets/Home.svg";
+import not from "../../Assets/Not.svg";
+import pfp from "../../Assets/Profile.svg";
+import list from "../../Assets/Lists.svg";
+import book from "../../Assets/Book.svg";
+import msg from "../../Assets/Msg.svg";
+import exp from "../../Assets/Expl.svg";
+import more from "../../Assets/More.svg";
+export const Sections = [
+  {
+    name: "Home",
+    path: "",
+    icon: home,
+  },
+  {
+    name: "Explore",
+    path: "explore",
+    icon: exp,
+  },
+  {
+    name: "Notifications",
+    path: "notifications",
+    icon: not,
+  },
+  {
+    name: "Messages",
+    path: "messages",
+    icon: msg,
+  },
+  {
+    name: "Bookmarks",
+    path: "bookmarks",
+    icon: book,
+  },
+  {
+    name: "Lists",
+    path: "lists",
+    icon: list,
+  },
+  {
+    name: "Profile",
+    path: "profile",
+    icon: pfp,
+  },
+  {
+    name: "More",
+    path: "more",
+    icon: more,
+  },
+];
